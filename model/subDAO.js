06fe7995789300c7ecfa99f2e7988b275bbdc7e1
shen <121836737@qq.com>
2018-10-23 21:46:31 +0800

    改二

--- model/subDAO.js
+++ model/subDAO.js
@@ -8,15 +8,15 @@ class DB{
         return DAO('select * from `subrecord` where priceId = ?',[Id])
     }
     sodeAdd(sav){
-        return DAO('insert into `subrecord` values(?,?,?,?)',
-            [sav.subId,sav.pId,sav.pcount,sav.pmoney]
+        return DAO('insert into `subrecord` values(?,?,?,?,?)',
+            [sav.subId,sav.pId,sav.pcount,sav.pmoney,sav.userId]
         )}
     sodedel(del){
         return DAO('delete from `subrecord` where subId=?',[del])
     }
     sodeup(up){
-        return DAO('update `subrecord`  set subId=?,pId=?,pcount=?,pmoney=?',
-            [up.subId,up.pId,up.pcount,up.pmoney])
+        return DAO('update `subrecord`  set subId=?,pId=?,pcount=?,pmoney=?,userId',
+            [up.subId,up.pId,up.pcount,up.pmoney,up.userId])
     }
 }
 module.exports = new DB();
